c755b9dffb05fb3e8f6e47408d5e757ccc86af1c
CarlosVF22 <CarlosVF22@users.noreply.github.com>
2021-10-06 22:47:30 +0000

    deploy: a560a2d21c9dee89962ccefbbfe457b83012e2de

---
+++ _dist_/index.js
@@ -0,0 +1,58 @@
+
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+const baseUrl = "https://platzi-avo.vercel.app";
+
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) =>{
+
+    const newPrice= new window.Intl.NumberFormat('en-En',{
+        style:"currency",
+        currency:"USD",
+    }).format(price)
+
+    return newPrice
+}
+
+//API Intl - internacionalizacion
+//1.fechas en este caso no usaremos la fecha
+//2.formato a monedas
+
+// web api
+// conectarnos al servidor
+// nos estamos conectando con promesas
+
+window
+    .fetch(`${baseUrl}/api/avo`)   
+    // procesar la respuesta y convertirla en JSON
+    .then(respuesta => respuesta.json())
+    //Json -> Data -> renderizar info en el browser
+    .then(responseJson => {
+        const todosLosItems =[];
+        responseJson.data.forEach((item) => {
+            //creando imagen
+            const imagen = document.createElement("img");
+            imagen.src = `${baseUrl}${item.image}`;
+            imagen.className="rounded-full"
+             //URL de la imagen
+            //crear titulo
+            const title = document.createElement("h2");
+            title.textContent = item.name;
+            title.className ="text-xl"; //utilizaremos framework tailwinds
+            //crear precio
+            const price = document.createElement("div");
+            //utilizando API internacional
+            price.textContent = formatPrice(item.price);
+
+            // Agregando las etiquetas creadas a un contenedor
+            const container = document.createElement("div");
+            container.className="shadow hover:bg-red-50"
+            container.append(imagen, title, price);
+
+            todosLosItems.push(container);
+        })
+        appNode.append(...todosLosItems);
+    });
